f4d725f9760c9699e8a901b098f1f1e0fbb20794
Rohit Gawade <103115917+rohitgawade1@users.noreply.github.com>
2023-08-10 20:00:23 +0530

    Update

--- src/Helper/BaseUrl.js
+++ src/Helper/BaseUrl.js
@@ -2,11 +2,11 @@ var url = window.location.origin
 export const BaseUrl =
     // url === "http://gayatriagroerp.gkmarket.in" ? "http://gayatriagroerpapi.gkmarket.in"
 
-         url === "http://demogayatriagroerp.gkmarket.in" ? "http://demogayatriagroerpapi.gkmarket.in"
-            : url === "http://gayatrierp.in" ? "http://api.gayatrierp.in" 
-            : url === "https://gayatrierp.in" ? "https://api.gayatrierp.in" :
+         // url === "http://demogayatriagroerp.gkmarket.in" ? "http://demogayatriagroerpapi.gkmarket.in"
+            // : url === "http://gayatrierp.in" ? "http://api.gayatrierp.in" 
+            // : url === "https://gayatrierp.in" ? "https://api.gayatrierp.in" :
                 // "http://api.gayatrierp.in"
-                "http://demogayatriagroerpapi.gkmarket.in"
+                // "http://demogayatriagroerpapi.gkmarket.in"
                 // "https://api.gayatrierp.in"
 
 
